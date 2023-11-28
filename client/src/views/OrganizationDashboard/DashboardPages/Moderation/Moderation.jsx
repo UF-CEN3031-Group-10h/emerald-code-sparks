230d2736c97002f762c14a3f5909cdc55bda7e7d
mkeloo <mkeloo@ufl.edu>
2023-11-28 09:32:27 -0500

    removed role switching from teacher login.

--- client/src/views/OrganizationDashboard/DashboardPages/Moderation/Moderation.jsx
+++ client/src/views/OrganizationDashboard/DashboardPages/Moderation/Moderation.jsx
@@ -1,28 +1,29 @@
 import React, { useState } from 'react';
 import '../Moderation/Moderation.css';
 import TeacherRole from './ModerationTeacher';
-import AdminRole from './ModerationAdmin';
+// import AdminRole from './ModerationAdmin';
 
 export default function OrganizationModeration() {
-  const [role, setRole] = useState('teacher'); // teacher or Admin
+  // const [role, setRole] = useState('teacher'); // teacher or Admin
 
-  const toggleRole = () => {
-    setRole(role === 'teacher' ? 'administrator' : 'teacher');
-  };
+  // const toggleRole = () => {
+  //   setRole(role === 'teacher' ? 'administrator' : 'teacher');
+  // };
 
   return (
     <div>
       <div id="main-header2">Teacher Moderation Tools</div>
       <div>
         <div>
-          <div className="switch-role">
-            <span className="switch-role-tag">Switch Role: </span>
+          {/* <div className="switch-role">
+            <span className="switch-role-tag">Current Role: </span>
             <button onClick={toggleRole} className="switch-role-btn">
               {role === 'teacher' ? 'Teacher' : 'Administrator'}
             </button>
-          </div>
-          {role === 'teacher' && <TeacherRole />}
-          {role === 'administrator' && <AdminRole />}
+          </div> */}
+          <TeacherRole />
+          {/* {role === 'teacher' && <TeacherRole />}
+          {role === 'administrator' && <AdminRole />} */}
         </div>
       </div>
     </div>
